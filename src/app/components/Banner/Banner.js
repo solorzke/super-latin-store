6098e8957585109281180acd9942168725eda674
Kevin Solorzano <solorzke@gmail.com>
2020-10-20 15:52:20 -0400

    Banner style set

--- src/app/components/Banner/Banner.js
+++ src/app/components/Banner/Banner.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './Banner.css';
 import Button from '../Button/Button';
+import Stormfront from '../../images/store.jpg';
 
 const Banner = ({ title, subtitle }) => {
 	return (
-		<div className="banner-bg">
-			<Container fluid>
-				<h1 className="banner-text">{title}</h1>
-				<h4 className="banner-text">{subtitle}</h4>
-				<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-					<Button label="Learn More" />
-				</div>
-			</Container>
+		<div className="wrap" style={{ fontFamily: "'Bebas Neue', cursive" }}>
+			<div className="banner-bg" style={{ backgroundImage: `url(${Stormfront})` }}>
+				<Container fluid>
+					<h1 className="banner-heading">
+						<strong>{title}</strong>
+					</h1>
+					<h4 className="banner-heading">
+						<strong>{subtitle}</strong>
+					</h4>
+					<div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+						<Button label="Learn More" />
+					</div>
+				</Container>
+			</div>
 		</div>
 	);
 };
