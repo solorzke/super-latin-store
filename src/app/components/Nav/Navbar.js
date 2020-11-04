dfc7c1c15880cae3d8ef085d81a0c8b0acc3b62c
Kevin Solorzano <solorzke@gmail.com>
2020-11-04 00:10:59 -0500

    Responsively lay out the collapsable menu

--- src/app/components/Nav/Navbar.js
+++ src/app/components/Nav/Navbar.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar, Nav, Col } from 'react-bootstrap';
 import { Links } from '../../data/Links';
 
 const Bar = ({ active }) => {
+	const [ adjust, setAdjust ] = useState(false);
+	useEffect(() => {
+		window.innerWidth > 993 ? setAdjust(true) : setAdjust(false);
+		const handleResize = () => {
+			switch (window.innerWidth > 993) {
+				case true:
+					if (!adjust) setAdjust(true);
+					break;
+				case false:
+					if (adjust) setAdjust(false);
+					break;
+			}
+		};
+		window.addEventListener('resize', handleResize);
+	});
+
 	const NavItems = Links.map((item, index) => {
 		return (
 			<Nav.Link active={active === item.name ? true : false} href={item.path} key={index}>
@@ -11,26 +27,52 @@ const Bar = ({ active }) => {
 		);
 	});
 
+	const Wrapper = ({ children }) => {
+		const Wrapped = () => <Col>{children}</Col>;
+		const Unwrapped = () => children;
+		return adjust ? <Wrapped /> : <Unwrapped />;
+	};
+
 	return (
 		<Navbar style={{ backgroundColor: '#7392B7', fontFamily: "'Bebas Neue', cursive" }} expand="lg" variant="dark">
-			<Col>
-				<Navbar.Brand href="/">
-					<span className="nav-brand">
-						<i className="fas fa-shipping-fast brand" /> Super Latin Store
-					</span>
-				</Navbar.Brand>
-			</Col>
-			<Col>
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="mx-auto parent">{NavItems}</Nav>
-				</Navbar.Collapse>
-			</Col>
-			<Col className="text-right">
-				<Navbar.Toggle
-					aria-controls="basic-navbar-nav"
-					children={<i style={{ color: '#fff' }} className="fas fa-align-right" />}
+			<Wrapper
+				children={
+					<Navbar.Brand href="/">
+						<span className="nav-brand">
+							<i className="fas fa-shipping-fast brand" /> Super Latin Store
+						</span>
+					</Navbar.Brand>
+				}
+			/>
+
+			{adjust && (
+				<Wrapper
+					children={
+						<Navbar.Collapse id="basic-navbar-nav">
+							<Nav className="mx-auto parent">{NavItems}</Nav>
+						</Navbar.Collapse>
+					}
 				/>
-			</Col>
+			)}
+
+			<Wrapper
+				children={
+					<Navbar.Toggle
+						aria-controls="basic-navbar-nav"
+						children={<i style={{ color: '#fff' }} className="fas fa-align-right" />}
+					/>
+				}
+			/>
+
+			{!adjust && (
+				<Wrapper
+					children={
+						<Navbar.Collapse id="basic-navbar-nav">
+							<Nav className="mx-auto parent">{NavItems}</Nav>
+						</Navbar.Collapse>
+					}
+				/>
+			)}
 		</Navbar>
 	);
 };
