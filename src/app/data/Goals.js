afc9c6b6ae7d177c6bb95ba155f5e82acb8c93ff
Kevin Solorzano <solorzke@gmail.com>
2020-11-19 20:28:20 -0500

    Goals text updated

--- src/app/data/Goals.js
+++ src/app/data/Goals.js
@@ -1,28 +1,32 @@
 export const ServiceText = [
 	{
-		service: 'Lorem Ipsum',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		icon: 'fas fa-bahai',
-		subtitle: 'Dolor sit amet, consectetur adipiscing',
+		service: 'Safe & Secure Money Transfer',
+		description: `Our trained employees can help you send your hard-earned cash to anyone around the world safely and securely.
+		Using our world class money transfer network, you can rest easy knowing your cash arrives to its destination on time and to the right person. The same applies
+		to anyone looking to receive money sent over from somewhere around the world. Pay us a visit with your documents ready to receive cash, on the spot.`,
+		icon: 'fas fa-money-bill-wave',
+		subtitle: 'Send/Receive Money easy & fast!',
 		sequence: 'first'
 	},
 
 	{
-		service: 'Lorem Ipsum',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		icon: 'fas fa-award',
-		subtitle: 'Dolor sit amet, consectetur adipiscing',
+		service: 'Everything You Need - Here',
+		description: `Our Bodega is equipped with almost everything a person could need last minute instead of driving many miles out. If you're in the mood
+		for a snack, we provide everything from chips, candy and crackers. We also provide imported goods from Ecuador including hominy, flax seeds, herbs, 
+		spices, beans, tuna, chuno flour, and much more. Feeling under the weather? - Visit our bodega and we can provide you with a selection of over-the-counter
+		medicine to treat flu, fever, headaches, injuries, and others.`,
+		icon: 'fas fa-store',
+		subtitle: 'Food, Drinks, Clothing and more available here.',
 		sequence: 'second'
 	},
 
 	{
-		service: 'Lorem Ipsum',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-		icon: 'fab fa-avianex',
-		subtitle: 'Dolor sit amet, consectetur adipiscing',
+		service: 'Reliability For All Your Financial Needs',
+		description: `Our reputation with our customers is very valuable to us, as we make sure to provide the best quality service in our community.
+		We're happy to assist you with any issues or questions you may have - ranging from planning your money transfer business, to providing financial recommendations
+		on your next visit. All you have to do, is ask!`,
+		icon: 'fas fa-hands-helping',
+		subtitle: 'Providing service to you any way we can.',
 		sequence: 'third'
 	}
 ];
@@ -55,7 +59,7 @@ export const BannerList = {
 };
 
 export const Label = {
-	title: 'Lorem Ipsum',
-	subtitle: 'Dapibus ac facilisis in',
+	title: 'Our Guarantee To You',
+	subtitle: 'Our Goals',
 	icon: 'fas fa-globe banner-title'
 };
