dab62c9e7c99902dc0c1da199e7dc48bac77bb9c
Kevin Solorzano <solorzke@gmail.com>
2020-11-22 23:29:38 -0500

    More text changes

--- src/app/data/Summary.js
+++ src/app/data/Summary.js
@@ -4,6 +4,9 @@ export const Summary = {
 		but have already made a great impact in our community by serving them with everything they need. Our business is more than just any convenience store,
 		we go above and beyond to providing you with the best service to send money overseas, across the world too. Our customers trust us to send over their 
 		hard earned cash in payment orders to whomever they wish at any time, using our business partners AFEX, & you can trust us too.`,
-		`Here at Super Latin Store, we have everything ranging from `
+		`We set up this store as a spot to help serve to the latin community that's rich in both culture and respect. 
+		As fellow Ecuadorians ourselves, we come to understand how much pride we take in the work we put into to serve to the community, at the highest level of 
+		dedication and care. Our store serves to provide you with a wide array of items and services, ranging from food, medicine and clothing, to more financial matters 
+		like money orders and bill payments. Pay us a visit soon with anything you need from us, we're there for you!`
 	]
 };
